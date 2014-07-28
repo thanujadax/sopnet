f4ef36faa4825d6c188aa819804788950b2a29c4
Thanuja Ambegoda <thanuja@ini.ch>
2014-07-28 16:09:19 +0200

    first implementation of delayed col generation for seg pairs

--- sopnet/sopnet/segments/SegmentPairDCG.cpp
+++ sopnet/sopnet/segments/SegmentPairDCG.cpp
@@ -10,18 +10,62 @@
 static logger::LogChannel segmentpairdcglog("segmentpairdcglog", "[SegmentPairDCG] ");
 
 SegmentPairDCG::SegmentPairDCG() :
-	_segmentPairsOut(boost::make_shared<Segments>()),
-	_segmentPairFeaturesOut(boost::make_shared<Features>()){
+	_segmentPairFeatureExtractor(boost::make_shared<SegmentPairFeatureExtractor>()),
+	_segmentPairSelector(boost::make_shared<SegmentPairSelector>()){
 
-	registerInput(_segmentPairsIn, "segment pairs");
+	registerInput(_segmentPairsIn, "segment pairs all");
 
-	registerOutput(_segmentPairsOut, "segment pairs DCG");
-	registerOutput(_segmentPairFeaturesOut, "segment pair features");
+	registerOutput(_segmentPairSelector->getOutput("segment pairs select"), "segment pairs select");
+
+	_segmentPairsIn.registerCallback(&SegmentPairDCG::onInputSet, this);
+
+	_segmentPairSelector->setInput("features",_segmentPairFeatureExtractor->getOutput());
+	_segmentPairSelector->setInput("segment pairs all",_segmentPairsIn);
+
+}
+
+
+SegmentPairDCG::SegmentPairSelector::SegmentPairSelector() :
+		_segmentPairsOut(boost::make_shared<Segments>()) {
+
+	registerInput(_features, "features");
+	registerInput(_segmentPairsIn,"segment pairs all");
+
+	registerOutput(_segmentPairsOut, "segment pairs select");
 
 }
 
 void
-SegmentPairDCG::updateOutputs(){
+SegmentPairDCG::SegmentPairSelector::updateOutputs(){
 /*	_segmentPairsOut->clear();
 	_segmentPairsOut->addSegments(_segmentPairsIn->getSegmentPairs());*/
+	selectSegmentPairs();
+
+
+}
+
+void
+SegmentPairDCG::onInputSet(const pipeline::InputSetBase&){
+	if (_segmentPairsIn.isSet()) {
+
+		_segmentPairFeatureExtractor->setInput("segments", _segmentPairsIn.getAssignedOutput());
+	}
+}
+
+
+void
+SegmentPairDCG::SegmentPairSelector::selectSegmentPairs(){
+
+	_segmentPairsOut->clear();
+
+	// select the segment pairs to use, based on feature values
+	foreach (boost::shared_ptr<SegmentPair> segmentPair, _segmentPairsIn->getSegmentPairs()) {
+
+		const std::vector<double>& features = _features->get(segmentPair->getId());
+
+		if(features[OFFSET_FEATURE_ID] < OFFSET_THRESHOLD){
+			_segmentPairsOut->add(segmentPair);
+		}
+
+	}
 }
--- sopnet/sopnet/segments/SegmentPairDCG.h
+++ sopnet/sopnet/segments/SegmentPairDCG.h
@@ -8,11 +8,17 @@
 #ifndef SOPNET_SEGMENTS_SEGMENTPAIRDCG_H_
 #define SOPNET_SEGMENTS_SEGMENTPAIRDCG_H_
 
+#define OFFSET_FEATURE_ID 60
+#define OFFSET_THRESHOLD 0.3
+
 #include <pipeline/all.h>
+#include <util/foreach.h>
+#include <imageprocessing/ImageStack.h>
 #include <sopnet/features/Features.h>
+#include <sopnet/features/SegmentPairFeatureExtractor.h>
 
 #include "Segments.h"
-
+#include "SegmentPair.h"
 
 class SegmentPairDCG : public pipeline::SimpleProcessNode<> {
 
@@ -21,16 +27,34 @@ public:
 
 private :
 
-	void updateOutputs();
+	class SegmentPairSelector : public pipeline::SimpleProcessNode<> {
 
-    // all segment pairs in the problem
-    pipeline::Input<Segments>          _segmentPairsIn;
+		public:
 
-    // selected segment pairs
-    pipeline::Output<Segments>          _segmentPairsOut;
+			SegmentPairSelector();
 
-    // all segments in the problem
-    pipeline::Output<Features>          _segmentPairFeaturesOut;
+		private:
+
+			void updateOutputs();
+
+			void selectSegmentPairs();
+
+			pipeline::Input<Segments> _segmentPairsIn;
+
+			pipeline::Input<Features> _features;
+
+			pipeline::Output<Segments> _segmentPairsOut;
+
+		};
+
+
+	void onInputSet(const pipeline::InputSetBase& signal);
+
+	pipeline::Input<Segments> _segmentPairsIn;
+
+    boost::shared_ptr<SegmentPairFeatureExtractor> _segmentPairFeatureExtractor;
+
+    boost::shared_ptr<SegmentPairSelector> _segmentPairSelector;
 
 };
 
