18073b4ab34af55782fad8b30b9f4372860d2a81
thanujadax <thanujadax@gmail.com>
2014-08-04 11:40:30 +0200

    new class for segmentPairEnds

---
+++ sopnet/sopnet/segments/SegmentPairEnd.cpp
@@ -0,0 +1,43 @@
+/*
+ * SegmentPairEnd.cpp
+ *
+ *  Created on: Aug 4, 2014
+ *      Author: thanuja
+ */
+
+
+#include <imageprocessing/ConnectedComponent.h>
+#include "SegmentPairEnd.h"
+
+SegmentPairEnd::SegmentPairEnd(unsigned int id, Direction direction,
+			boost::shared_ptr<ContinuationSegment> continuationSegment,
+			boost::shared_ptr<ContinuationSegment> endSegment) :
+		Segment(
+				id,
+				direction,
+				continuationSegment->getSourceSlice()->getComponent()->getCenter(),
+				 continuationSegment->getInterSectionInterval()),
+		_continuationSegment(continuationSegment),
+		_endSegment(endSegment) {}
+
+boost::shared_ptr<ContinuationSegment>
+SegmentPairEnd::getContinuationSegment() const {
+
+	return _continuationSegment;
+}
+
+boost::shared_ptr<EndSegment>
+SegmentPairEnd::getEndSegment() const {
+
+	return _endSegment;
+}
+
+std::vector<boost::shared_ptr<Slice> >
+SegmentPairEnd::getSlices() const {
+
+	std::vector<boost::shared_ptr<Slice> > slices;
+
+	//slices.push_back(getSlice());
+
+	return slices;
+}
---
+++ sopnet/sopnet/segments/SegmentPairEnd.h
@@ -0,0 +1,39 @@
+/*
+ * SegmentPairEnd.h
+ *
+ *  Created on: Aug 4, 2014
+ *      Author: thanuja
+ */
+
+#ifndef SEGMENTPAIREND_H_
+#define SEGMENTPAIREND_H_
+
+#include "Segment.h"
+#include "ContinuationSegment.h"
+#include "EndSegment.h"
+
+class SegmentPairEnd: public Segment {
+
+public:
+
+	SegmentPairEnd(unsigned int id, Direction direction,
+			boost::shared_ptr<ContinuationSegment> continuationSegment,
+			boost::shared_ptr<ContinuationSegment> endSegment);
+
+	boost::shared_ptr<ContinuationSegment> getContinuationSegment() const;
+
+	boost::shared_ptr<EndSegment> getEndSegment() const;
+
+	std::vector<boost::shared_ptr<Slice> > getSlices() const;
+
+private:
+
+	boost::shared_ptr<ContinuationSegment> _continuationSegment;
+
+	boost::shared_ptr<EndSegment> _endSegment;
+
+};
+
+
+#endif /* SEGMENTPAIREND_H_ */
+
